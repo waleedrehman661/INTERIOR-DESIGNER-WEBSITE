7280c3ac307c314c10b9e5dcbad4186c27e5f13c
Waleed-Rehman <waleedrehman2007@gmail.com>
2025-01-16 06:17:33 +0500

    projects file done

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -7,9 +7,28 @@ var swiper = new Swiper(".mySwiper", {
       clickable: true,
     },
   });
-
-  
+ 
 const scroll = new LocomotiveScroll({
-  el: document.querySelector('body'),
+  el: document.querySelector('#main-container'),
   smooth: true
 });
+
+// MODELS
+$.getJSON("assets/js/data.json", function (products) {
+  console.log(products);  
+  var cardsContainer = $("#projects");
+  products.forEach(product => {
+    const card = `
+      <div class="project-item">
+        <img src="${product.image}" alt="${product.name}">
+        <div class="overlay">
+          <h3>${product.name}</h3>
+        </div>
+      </div>
+    `;
+    cardsContainer.append(card);
+  });
+});
+
+
+
